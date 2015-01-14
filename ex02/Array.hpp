fc03872a1769d8a325e4bf32a4fecee75177dcb5
Ogrepousse <etienne_99@hotmail.com>
2015-01-14 21:16:56 +0100

    ex02 in progress

---
+++ ex02/Array.hpp
@@ -0,0 +1,50 @@
+#ifndef ARRAY_H
+# define ARRAY_H
+
+# include <cstdlib>
+# include <stdexcept>
+
+template <typename T>
+class Array
+{
+	public:
+		Array() { _tab = NULL; _n = 0;};
+		Array(int n) { _tab = new T[n]; _n = n; };
+		~Array() { delete _tab; };
+		Array(const Array & src) {
+			*this = src;
+ 		};
+
+		T & operator[](int i) {
+			if (i >= _n)
+				throw std::out_of_range("index");
+			return _tab[i];
+		};
+
+		Array & operator=(const Array & rhs) {
+			delete _tab;
+			_tab = NULL;
+			_n = rhs.size();
+			if (_n)
+			{
+				_tab = new T[_n];
+				for (int i = 0; i < _n; i++)
+				{
+					_tab[i] = rhs[i];
+				}
+			}
+			else
+				_tab = NULL;
+			return *this;
+		};
+
+
+
+
+		int size() const { return _n; };
+	private:
+		T * _tab;
+		int _n;
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,16 @@
+#include "Array.hpp"
+#include <iostream>
+
+int		main()
+{
+	Array<int>	tab(2);
+	tab[0] = 1;
+	tab[2] = 2;
+	Array<int>	t(tab);
+	Array<int>	t2;
+	t2 = tab;
+	std::cout << tab[0] << " " << tab[1] << std::endl;
+	std::cout << t[0] << " " << t[1] << std::endl;
+	std::cout << t2[0] << " " << t2[1] << std::endl;
+	return (0);
+}
